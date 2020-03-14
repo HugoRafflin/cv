520e3e074fdfa8025c7da495de6093efa3a045e5
Isten <hugo.rafflin@laposte.net>
2020-03-14 13:24:30 +0100

    first commit

---
+++ sendEmail.php
@@ -0,0 +1,69 @@
+﻿<?php
+
+//Adresse du destinataire
+$siteOwnersEmail = 'hugo.rafflin@laposte.net';
+
+
+if($_POST) {
+
+   $name = trim(stripslashes($_POST['contactName']));
+   $email = trim(stripslashes($_POST['contactEmail']));
+   $subject = trim(stripslashes($_POST['contactSubject']));
+   $contact_message = trim(stripslashes($_POST['contactMessage']));
+
+   // Vérification du nom
+	if (strlen($name) < 2) {
+		$error['name'] = "S'il vous plait entrez votre nom.";
+	}
+	// Vérification de l'email
+	if (!preg_match('/^[a-z0-9&\'\.\-_\+]+@[a-z0-9\-]+\.([a-z0-9\-]+\.)*+[a-z]{2}/is', $email)) {
+		$error['email'] = "S'il vous plait entrez votre addresse email.";
+	}
+	// Vérification du message
+	if (strlen($contact_message) < 15) {
+		$error['message'] = "S'il vous plait entrez votre message. Il doit contenir au moins 15 caractères.";
+	}
+   // Subject
+	if ($subject == '') { $subject = "Contact Form Submission"; }
+
+
+   // Set Message
+   $message .= "Email from: " . $name . "<br />";
+   $message .= "Email address: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= $contact_message;
+   $message .= "<br /> ----- <br /> This email was sent from your site's contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $email . "\r\n";
+ 	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+	// Si il y a une erreur 
+   if (!$error) {
+
+      ini_set("sendmail_from", $siteOwnersEmail); // Pour les serveurs windows
+      $mail = mail($siteOwnersEmail, $subject, $message, $headers);
+
+		if ($mail) { echo "OK"; }
+      else { echo "Something went wrong. Please try again."; }
+		
+	}
+	// Si il n'y a pas d'erreurs
+	else {
+
+		$response = (isset($error['name'])) ? $error['name'] . "<br /> \n" : null;
+		$response .= (isset($error['email'])) ? $error['email'] . "<br /> \n" : null;
+		$response .= (isset($error['message'])) ? $error['message'] . "<br />" : null;
+		
+		echo $response;
+
+	}
+
+}
+
+?>
